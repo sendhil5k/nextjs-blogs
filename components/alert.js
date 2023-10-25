787b653d0e12c3e3d1a6e6c3dc3b614001aac074
sendhil kumar <sendhil5k@gmail.com>
2023-10-25 13:01:26 +0530

    third commit

--- components/alert.js
+++ components/alert.js
@@ -7,6 +7,6 @@ export default function Alert(children){
         [styles.error]: type === 'error',
         })}>
             <strong>{children}</strong>
-        </div>   
+        </div>
     )
 }
